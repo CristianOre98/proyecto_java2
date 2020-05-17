3f0b16e9f3a5149cd266f1ce6ad29c67e8603eae
CristianOre98 <crisvlaore98@gmail.com>
2020-05-16 18:11:15 -0600

    he agregado el codigo a la clase suma

--- proyecto_java2/src/suma/suma.java
+++ proyecto_java2/src/suma/suma.java
@@ -14,7 +14,23 @@ import java.util.Scanner;
 public class suma {
    
           public static void main(String[] args) {
+            
         
+        Scanner leer = new Scanner(System.in);
+        
+    double n1, n2, result;
+    
+        System.out.println("******* Suma ***********");
+        System.out.println("Ingrese el primer numero");
+        n1 = leer.nextDouble();
+        System.out.println("Ingrese el primer numero");
+        n2 = leer.nextDouble();
+        
+        result = n1 * n2;
+        System.out.println("*************************");
+        System.out.println("**** resultado de la suma es: "+result);
+        System.out.println("*************************");
+   
             
     
     }
